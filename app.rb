b2f5e94ba9bbb3044f06608b04e429838e46bbda
Erin <erinpwoo@yahoo.com>
2021-01-15 23:14:39 -0800

    basic get

--- app.rb
+++ app.rb
@@ -3,20 +3,28 @@ require 'pp' # used for printing
 
 get '/' do
   PP.pp request # printing request
-  "GET/\n"
+  redirect to('/files/')
+  status 302
+  "location: " + request.path_info + "/files/"
+  "GET /\n"
 end
 
 get '/files/' do
   PP.pp request # printing request
-  "GET/files/\n"
+  "GET /files/\n"
 end
 
 get '/files/:digest' do |file|
   PP.pp request # printing request
-  "GET/files/{DIGEST}\n"
+  "GET /files/{DIGEST}\n"
 end
 
-post '/files' do
+post '/files/' do
   PP.pp request # printing request
   "POST\n"
+end
+
+delete '/files/:digest' do |file|
+  PP.pp request # printing request
+  "DELETE \n"
 end
