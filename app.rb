5297a9b70858c4664ca4a5b55052b21fc924c197
Erin <erinpwoo@yahoo.com>
2021-01-15 22:57:37 -0800

    basic route setup

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
+require 'pp' # used for printing
 
 get '/' do
-  "Hello World\n"
+  PP.pp request # printing request
+  "GET/\n"
 end
 
-post '/' do
-  require 'pp'
-  PP.pp request
+get '/files/' do
+  PP.pp request # printing request
+  "GET/files/\n"
+end
+
+get '/files/:digest' do |file|
+  PP.pp request # printing request
+  "GET/files/{DIGEST}\n"
+end
+
+post '/files' do
+  PP.pp request # printing request
   "POST\n"
 end
