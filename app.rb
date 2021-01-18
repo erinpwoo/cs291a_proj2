171de61f793991dac6feb726fcb9a8ebc60b247f
Erin <erinpwoo@yahoo.com>
2021-01-17 19:17:09 -0800

    hexdigest post

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 require 'pp' # used for printing
 require'google/cloud/storage'
+require 'digest'
 
 # setting up gcs bucket
-storage = Google:Cloud::Storage.new(project_id: cs291a)
-bucket = storage.bucket 'cs291project2', skip_lookup=true
+storage = Google::Cloud::Storage.new(project_id: 'cs291a')
+bucket = storage.bucket 'cs291project2', skip_lookup: true
 
 get '/' do
   PP.pp request # printing request
@@ -26,6 +27,11 @@ end
 
 post '/files/' do
   PP.pp request # printing request
+  file = File.open params['file']['tempfile']
+  digest = Digest::SHA256.hexdigest file.read
+  name = digest.insert(2, '/')
+  name = name.insert(5, '/')
+  
   "POST\n"
 end
 
