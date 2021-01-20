e26d1b4c2ffe1fb44de444d8afbb247113a09a1c
Erin <erinpwoo@yahoo.com>
2021-01-20 01:07:36 -0800

    need to sort file list

--- app.rb
+++ app.rb
@@ -33,19 +33,24 @@ get '/files/:digest' do |file|
   if regex == nil
     return 422
   end
-  if regex.length != file.length
+  if regex.to_s.length != file.length
+    lengths = regex.length.to_s + " " + file.length.to_s
+    body lengths
     return 422
   end
-  path = file.insert(2, '/')
-  path = path.insert(5, '/')
-  if bucket.file(path) == nil
-    puts path
+  filename = String.new(file)
+  filename.insert(2, '/')
+  path = filename.insert(5, '/')
+  path = path.downcase
+  if !bucket.file(path)&.exists?
+    body path
     return 404
   end
   downloaded = bucket.file(path)
   content = downloaded.download
-  header = {"Content-Type" => downloaded.content_type}
   data = content.read
+  content.rewind
+  header = {"Content-Type" => downloaded.content_type}
   return [200, header, data.to_s]
 end
 
@@ -54,31 +59,42 @@ post '/files/' do
     return 422
   end
   if params['file'] == nil
+    body "line 57"
     return 422
   end
   if params['file']['tempfile'] == nil
+    body "line 60"
+    return 422
+  end
+
+  if (!File.file?(params['file']['tempfile'])) 
+    body "line 69"
     return 422
   end
   file_size = File.size(params['file']['tempfile'])
-  if file_size == 0 || file_size > 1000000
+  if file_size > 1048576
+    body file_size.to_s
     return 422
   end
   if !File.readable?(params['file']['tempfile'])
+    body "line 73"
     return 422
   end
   file = File.open params['file']['tempfile']
   digest = Digest::SHA256.hexdigest file.read
   path = digest.insert(2, '/')
-  path = path.insert(5, '/')
+  path.insert(5, '/')
+  path = path.downcase
   if bucket.file(path)&.exists?
     halt 409
     return
   end
   file.rewind
   file_name = Digest::SHA256.hexdigest file.read
+  puts params
   puts file_name
   file.rewind
-  bucket.upload_file(file, path)
+  bucket.upload_file(file, path, content_type: params['file']['type'])
   json = {'uploaded': file_name}
   puts JSON.generate(json)
   return [201, JSON.generate(json)]
@@ -89,11 +105,14 @@ delete '/files/:digest' do |file|
   if regex == nil
     return 422
   end
-  if regex.length != file.length
+  if regex.to_s.length != file.length
+    lengths = regex.length.to_s + " " + file.length.to_s
+    body lengths
     return 422
   end
-  path = file.insert(2, '/')
-  path = path.insert(5, '/')
+  path = String.new(file.downcase)
+  path.insert(2, '/')
+  path.insert(5, '/')
   file = bucket.file path
   if file != nil
     file.delete
